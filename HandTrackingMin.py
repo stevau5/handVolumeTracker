039c82f493e361044fee49a8c50800c1bb91fa66
stevau5 <stevenanthonyzanga@gmail.com>
2021-03-30 22:44:14 -0400

    minimal hand tracking

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -4,11 +4,13 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 
 while True:
     success, img = cap.read()
-
-
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
