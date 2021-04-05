835e6628a1b46c4846a5705634143184be268cd4
stevau5 <stevenanthonyzanga@gmail.com>
2021-04-05 18:37:56 -0400

    time tracking

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,15 +8,30 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
 
     # if hands are detected, for each hand ...
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # destruct shape into height width and chanel
+                h, w, c = img.shape
+                #lm in pixel location instead of ratio
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    # fps
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
